8ef0e4642cbea3742b3ed977b5ee654b4536dd72
Lam Nguyen <motniemtin@gmail.com>
2019-05-03 00:47:13 +0200

    first

--- src/Motniemtin/Phash/Phash.php
+++ src/Motniemtin/Phash/Phash.php
@@ -1,9 +1,7 @@
 <?php
-namespace Motniemtin\Auto;
-
-use Motniemtin\Auto\AutoProxy;
+namespace Motniemtin\Phash;
 use Exception;
-class pHash{
+class Phash{
     protected $bitCounts = array(0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,
                                  2,3,3,4,2,3,3,4,3,4,4,5,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,
                                  4,3,4,4,5,3,4,4,5,4,5,5,6,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,
